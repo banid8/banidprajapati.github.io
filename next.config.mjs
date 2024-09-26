1ea013005b292726c6a9ba20e4841296093a1d55
Banid Prajapati <74712385+banidprajapati@users.noreply.github.com>
2024-09-26 08:26:33 +0545

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,6 @@
 // next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  basePath: process.env.NODE_ENV === "production" ? "" : undefined,
-  experimental: {
-    appDir: true,
-  },
-  images: {
-    unoptimized: true, // Allow images to be used without optimization for easier export
-  },
-  reactStrictMode: true,
 };
 
 export default nextConfig;
